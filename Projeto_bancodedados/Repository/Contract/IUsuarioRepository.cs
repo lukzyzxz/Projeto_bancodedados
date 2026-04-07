480bcaa476826fbf5167b088cf4e2239c1556dd7
lukzyzxz <lucasfeliperibeiroqueiroz777@gmail.com>
2026-04-06 21:10:41 -0300

    feat

--- Projeto_bancodedados/Repository/Contract/IUsuarioRepository.cs
+++ Projeto_bancodedados/Repository/Contract/IUsuarioRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace Projeto_bancodedados.Repository.Contract
+﻿using Projeto_bancodedados.Models;
+
+namespace Projeto_bancodedados.Repository.Contract
 {
     public interface IUsuarioRepository
     {
+        //CRUD
+        IEnumerable<Usuario> ObterTodosUsuarios();
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(Usuario usuario);
+
+        Usuario ObterUsuario(int id);
+
+        void Excluir(int id);
     }
 }
---
+++ Projeto_bancodedados/Repository/UsuarioRepository.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_bancodedados.Models;
+using Projeto_bancodedados.Repository.Contract;
+
+namespace Projeto_bancodedados.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public UsuarioRepository(IConfiguration conf)
+        {
+            _conexaoMySQL = conf.GetConnectionString("ConexãoMySQL");
+            
+        }
+        public void Atualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into usuario(nomeUsu, Cargo, DataNasc) " +
+                                                " values (@nomeUsu, @Cargo, @DataNasc )", conexao); // @: PARAMETRO
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.cargo;
+                // cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.DataNasc.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.dataNasc.ToString("yyyy/MM/dd");
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObterUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
